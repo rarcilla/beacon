50bd35fbc210b4d59385b2932ebcfdc2450232a2
Regina <rlarcilla@hotmail.com>
2020-04-08 18:36:37 -0700

    reorganized chatroomviewcontroller

--- Beacon/ChatRoomViewController.swift
+++ Beacon/ChatRoomViewController.swift
@@ -38,28 +38,108 @@ class ChatRoomViewController: MessagesViewController, MCSessionDelegate, MCNearb
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messagesCollectionView.messagesDataSource = self
-        messagesCollectionView.messagesLayoutDelegate = self
-        messagesCollectionView.messagesDisplayDelegate = self
-        
+        setupMPC()
+        setupMessages()
         setupMessageInputBar()
         removeAvatar()
+    }
+}
 
-        self.navigationItem.title = "Chatroom (0 peers)"
+//extension for Multipeer Connectivity
+extension ChatRoomViewController {
+    
+    func setupMPC() {
+        navigationItem.title = "Chatroom (0 peers)"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showMenu))
         
         session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         session?.delegate = self
     }
+
+    // MARK: - MCSessionDelegate Methods
     
-    func removeAvatar() {
-        if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
-          layout.setMessageIncomingAvatarSize(.zero)
-          layout.setMessageOutgoingAvatarSize(.zero)
-          layout.setMessageIncomingMessageTopLabelAlignment(LabelAlignment(textAlignment: .left, textInsets: UIEdgeInsets(top: 0, left: 18, bottom: 0, right: 0)))
-          layout.setMessageOutgoingMessageTopLabelAlignment(LabelAlignment(textAlignment: .right, textInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 18)))
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("\(peerID) has connected.")
+            updateTitle()
+        case .connecting:
+            print("\(peerID) is connecting.")
+        case .notConnected:
+            print("\(peerID) is not connected.")
+            updateTitle()
+        @unknown default:
+            print("state of \(peerID) is unknown.")
         }
     }
+
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async { [weak self] in
+            if let messageString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
+                
+                let user = Sender(senderId: String(describing: peerID), displayName: peerID.displayName)
+                
+                let message = Message(sender: user, messageId: UUID().uuidString, sentDate: Date(), kind: .text(messageString as String))
+                
+                self?.insertMessage(message)
+            }
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+    // MARK: - MCBrowserViewControllerDelegate Methods
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    // MARK: - MCNearbyServiceAdvertiserDelegate Methods
+    
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        invitationHandler(true, session)
+    }
+    
+    // MARK: - Hosting/Joining Chat Methods
+    
+    func hostSession(_ action: UIAlertAction) {
+        advertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: appServiceType)
+        advertiser.delegate = self
+        advertiser.startAdvertisingPeer()
+    }
+    
+    func joinSession(_ action: UIAlertAction) {
+        guard let session = session else { return }
+        let browser = MCBrowserViewController(serviceType: appServiceType, session: session)
+        browser.delegate = self
+        present(browser, animated: true)
+    }
+    
+    @objc func showMenu() {
+        let alert = UIAlertController(title: "Chat Options", message: nil, preferredStyle: .alert)
+        
+        let hostAction = UIAlertAction(title: "Host a chat", style: .default, handler: hostSession)
+        let joinAction = UIAlertAction(title: "Join a chat", style: .default, handler: joinSession)
+        
+        alert.addAction(hostAction)
+        alert.addAction(joinAction)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(alert, animated: true)
+    }
+    
+    //MARK: - Other methods
     
     func sendMessage() {
         guard let session = session else { return }
@@ -97,119 +177,14 @@ class ChatRoomViewController: MessagesViewController, MCSessionDelegate, MCNearb
     }
 }
 
-//extension for MPC
-extension ChatRoomViewController {
-        // MARK: - MCSessionDelegate Methods
-        func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
-            switch state {
-            case .connected:
-                print("\(peerID) has connected.")
-                updateTitle()
-            case .connecting:
-                print("\(peerID) is connecting.")
-            case .notConnected:
-                print("\(peerID) is not connected.")
-                updateTitle()
-            @unknown default:
-                print("state of \(peerID) is unknown.")
-            }
-        }
-    
-        func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-            DispatchQueue.main.async { [weak self] in
-                if let messageString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
-                    
-                    let user = Sender(senderId: String(describing: peerID), displayName: peerID.displayName)
-                    
-                    let message = Message(sender: user, messageId: UUID().uuidString, sentDate: Date(), kind: .text(messageString as String))
-                    
-                    self?.insertMessage(message)
-                }
-            }
-        }
-
-        func insertMessage(_ message: Message) {
-            messages.append(message)
-
-            messagesCollectionView.performBatchUpdates({
-                messagesCollectionView.insertSections([messages.count - 1])
-                if messages.count >= 2 {
-                    messagesCollectionView.reloadSections([messages.count - 2])
-                }
-            }, completion: { [weak self] _ in
-                if self?.isLastSectionVisible() == true {
-                    self?.messagesCollectionView.scrollToBottom(animated: true)
-                }
-            })
-        }
-    
-        
-        func isLastSectionVisible() -> Bool {
-            
-            guard !messages.isEmpty else { return false }
-            
-            let lastIndexPath = IndexPath(item: 0, section: messages.count - 1)
-            
-            return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
-        }
-        
-
-        func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        }
-        
-        func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
-        }
-        
-        func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
-        }
-        
-        // MARK: - MCBrowserViewControllerDelegate Methods
-        
-        func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-            dismiss(animated: true)
-        }
-        
-        func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-            dismiss(animated: true)
-        }
-        
-        // MARK: - MCNearbyServiceAdvertiserDelegate Methods
-        
-        func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
-            invitationHandler(true, session)
-        }
-        
-        // MARK: - Hosting/Joining Chat Methods
-        
-        func hostSession(_ action: UIAlertAction) {
-            advertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: nil, serviceType: appServiceType)
-            advertiser.delegate = self
-            advertiser.startAdvertisingPeer()
-        }
-        
-        func joinSession(_ action: UIAlertAction) {
-            guard let session = session else { return }
-            let browser = MCBrowserViewController(serviceType: appServiceType, session: session)
-            browser.delegate = self
-            present(browser, animated: true)
-        }
-        
-        @objc func showMenu() {
-            let alert = UIAlertController(title: "Chat Options", message: nil, preferredStyle: .alert)
-            
-            let hostAction = UIAlertAction(title: "Host a chat", style: .default, handler: hostSession)
-            let joinAction = UIAlertAction(title: "Join a chat", style: .default, handler: joinSession)
-            
-            alert.addAction(hostAction)
-            alert.addAction(joinAction)
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-            
-            present(alert, animated: true)
-        }
-}
-
 //extension for MessageKit
 extension ChatRoomViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    
+    func setupMessages(){
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+    }
     
     //MARK: - MessagesDataSource Methods
     func currentSender() -> SenderType {
@@ -275,6 +250,39 @@ extension ChatRoomViewController: MessagesDataSource, MessagesLayoutDelegate, Me
     
     func configureAvatarView(_ avatarView: AvatarView, for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) {
       avatarView.isHidden = true
+    }
+    
+    func removeAvatar() {
+        if let layout = messagesCollectionView.collectionViewLayout as? MessagesCollectionViewFlowLayout {
+          layout.setMessageIncomingAvatarSize(.zero)
+          layout.setMessageOutgoingAvatarSize(.zero)
+          layout.setMessageIncomingMessageTopLabelAlignment(LabelAlignment(textAlignment: .left, textInsets: UIEdgeInsets(top: 0, left: 18, bottom: 0, right: 0)))
+          layout.setMessageOutgoingMessageTopLabelAlignment(LabelAlignment(textAlignment: .right, textInsets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 18)))
+        }
+    }
+    
+    func insertMessage(_ message: Message) {
+        messages.append(message)
+
+        messagesCollectionView.performBatchUpdates({
+            messagesCollectionView.insertSections([messages.count - 1])
+            if messages.count >= 2 {
+                messagesCollectionView.reloadSections([messages.count - 2])
+            }
+        }, completion: { [weak self] _ in
+            if self?.isLastSectionVisible() == true {
+                self?.messagesCollectionView.scrollToBottom(animated: true)
+            }
+        })
+    }
+    
+    func isLastSectionVisible() -> Bool {
+        
+        guard !messages.isEmpty else { return false }
+        
+        let lastIndexPath = IndexPath(item: 0, section: messages.count - 1)
+        
+        return messagesCollectionView.indexPathsForVisibleItems.contains(lastIndexPath)
     }
 }
 
